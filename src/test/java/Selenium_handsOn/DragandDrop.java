759f76d2c3beb1b4f211e0c7d77054372220b406
C <sakthivigneshc3@gmail.com>
2024-07-29 23:15:54 +0530

    DragAndDropConcept

---
+++ src/test/java/Selenium_handsOn/DragandDrop.java
@@ -0,0 +1,32 @@
+package Selenium_handsOn;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DragandDrop {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://letcode.in/dropable");
+
+		WebElement draggable = driver.findElement(By.id("draggable"));
+		WebElement droppable = driver.findElement(By.id("droppable"));
+
+		Actions actions = new Actions(driver);
+		actions.dragAndDrop(draggable, droppable).build().perform();
+
+		WebElement elementText = driver.findElement(By.xpath("//div[@id='droppable']/p"));
+		String text = elementText.getText(); 
+		System.out.println(text);// Print the text after dropped the box 
+		driver.quit();
+
+	}
+
+}
